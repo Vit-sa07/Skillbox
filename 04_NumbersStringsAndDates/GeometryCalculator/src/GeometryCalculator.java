e2a9784eb2fe62b702cc47b4ab80786043108bd3
Виталик <vitalik@Air-Vitalik.Dlink>
2020-08-11 22:57:31 +0300

    lesson 4.3 remade

--- 04_NumbersStringsAndDates/GeometryCalculator/src/GeometryCalculator.java
+++ 04_NumbersStringsAndDates/GeometryCalculator/src/GeometryCalculator.java
@@ -12,18 +12,21 @@ public class GeometryCalculator {
     }
 
     public static boolean isTriangleRightAngled(double a, double b, double c) {
-        if (a + b > c && a + c > b && b + c > a) {
-            return true;
-        } else {
-            return false;
-        }
+        return a < b + c && b < a + c && c < a + b;
     }
 
     // перед расчетом площади рекомендуется проверить возможен ли такой треугольник
     // методом isTriangleRightAngled, если невозможен вернуть -1.0
     public static double getTriangleSquare(double a, double b, double c) {
-        double p = (a + b + c)/2;
-        double area = Math.sqrt(p * (p - a) * (p - b) * (p - c));
-        return area;
+        if (isTriangleRightAngled(a, b, c)) {
+            double p = (a + b + c) / 2;
+            double p1 = (p - a);
+            double p2 = (p - b);
+            double p3 = (p - c);
+            double pp = p * p1 * p2 * p3;
+            double s = Math.sqrt(pp);
+            return s;
+        }
+        return -1.0;
     }
 }
--- 04_NumbersStringsAndDates/TrucksContainer/src/Main.java
+++ 04_NumbersStringsAndDates/TrucksContainer/src/Main.java
@@ -2,14 +2,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int box = 0;
+
+        final int CONTAINERS_IN_TRUCK = 12;
+        final int BOXES_IN_CONTAINER = 27;
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите количество коробок:");
-        box = scanner.nextInt();
-        Cargo cargo = new Cargo(box);
-        cargo.result();
+        int boxCount = scanner.nextInt();
+
+        final int BOX_IN_TRUCK = BOXES_IN_CONTAINER * CONTAINERS_IN_TRUCK;
+
+        for (int box = 0; box < boxCount; box++) {
+            if (box % BOXES_IN_CONTAINER == 0) {
+                if (box % BOX_IN_TRUCK == 0) {
+                    System.out.println("Грузовик " + (box / BOX_IN_TRUCK + 1));
+                }
+                System.out.println("Контейнер " + (box / BOXES_IN_CONTAINER + 1));
+            }
+            System.out.println("Ящик " + (box + 1));
+        }
     }
 }
+
 
 
 
