3217931bf408681fed8019e2366f07c215b67ee2
Виталик <vitalik@Air-Vitalik.Dlink>
2020-08-17 13:47:10 +0300

    lesson 4.6 remade

--- 04_NumbersStringsAndDates/Birthday/src/Main.java
+++ 04_NumbersStringsAndDates/Birthday/src/Main.java
@@ -1,30 +1,17 @@
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 public class Main {
-    public static void main( String[] args )
-    {
-        int day = 5;
-        int month = 4;
-        int year = 1994;
-        Locale localeRu = new Locale("ru", "RU");
-        DateFormat dateFormat = new SimpleDateFormat("- dd.MM.yyyy - EEE", localeRu);
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(year, month, day);
-//        calendar.set(1994, Calendar.MAY, 5);
-        Date date = new Date();
-        Date birth = calendar.getTime();
-        int i = 0;
-        while (date.compareTo(birth) > 0)
-        {
-            System.out.println(i + dateFormat.format(birth));
-            calendar.add(Calendar.YEAR, 1);
-            birth = calendar.getTime();
-            i++;
+    public static void main(String[] args) {
 
+        LocalDate birthday = LocalDate.of(1994, 5, 5);
+        LocalDate today = LocalDate.now();
+        int i = 0;
+        for (LocalDate date = birthday; date.isBefore(today); date = date.plusYears(1)) {
+            DateTimeFormatter printFormat = DateTimeFormatter.ofPattern("dd.MM.yyyy E", new Locale("ru"));
+            System.out.println(i + " - " + date.format(printFormat));
+            i++;
         }
     }
 }
