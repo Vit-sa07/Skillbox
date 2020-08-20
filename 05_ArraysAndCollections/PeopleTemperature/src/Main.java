1d72e91c020a575698c7dd5c84a20a91b74baffd
Виталик <vitalik@Air-Vitalik.Dlink>
2020-08-20 20:56:30 +0300

    lesson 5.1 remade

--- 05_ArraysAndCollections/PeopleTemperature/src/Main.java
+++ 05_ArraysAndCollections/PeopleTemperature/src/Main.java
@@ -1,3 +1,5 @@
+import java.text.DecimalFormat;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -11,18 +13,19 @@ public class Main {
         float[] array = new float [PATIENTS_COUNT];
         float s = 0; 
         int healty = 0;
-
         for (int i = 0; i < array.length; i++) {
-            array[i] = 36 + (float) (Math.random() * 5);
+            array[i] = 36 + (float) (Math.random() * 5) ;
+            String formattedDouble = new DecimalFormat("#0.00").format(array[i]);
             if (array[i] >= MIN_PERMISS && array[i] <= MAX_PERMISS) {
                 healty++;
             }
             s += array[i];
 
-            System.out.println("Температура пациента - " + array[i]);
-
+            System.out.println("Температура пациента - " + formattedDouble);
         }
-        System.out.println("Средняя температура по больнице " + s / array.length);
+        float a = s / array.length;
+        float roundOff = Math.round(a * 100.0f) / 100.0f;
+        System.out.println("Средняя температура по больнице " + roundOff);
         System.out.println("Количество здоровых пациентов " + healty);
     }
 }
--- 05_ArraysAndCollections/RainbowColor/src/Main.java
+++ 05_ArraysAndCollections/RainbowColor/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String[] text = { "Каждый "," охотник "," желает "," знать "," где "," сидит "," фазан "};
+        String[] text = { " фазан"," сидит"," где"," знать"," желает"," охотник","Каждый "};
         for (int i = text.length-1; i >= 0; i--)
             System.out.print(text[i]);
     }
