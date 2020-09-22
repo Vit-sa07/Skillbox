9f18deb8bf14f326808985a1a41bdfa4bd230c93
Vit-sa07 <57671251+Vit-sa07@users.noreply.github.com>
2020-09-22 16:03:50 +0300

    did homework 7.2

--- 07_AdvancedOOPFeatures/LambdaExpressions/src/Main.java
+++ 07_AdvancedOOPFeatures/LambdaExpressions/src/Main.java
@@ -13,6 +13,11 @@ public class Main
     public static void main(String[] args)
     {
         ArrayList<Employee> staff = loadStaffFromFile();
+        Collections.sort(staff, Comparator.comparing(Employee::getSalary)
+                .thenComparing(Employee::getName));
+        for (Employee employee : staff){
+            System.out.println(employee);
+        }
 
 
     }
