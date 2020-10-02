33008fb2b7b629e3e8e6665d4b05dff3b59dc0a9
Vit-sa07 <57671251+Vit-sa07@users.noreply.github.com>
2020-10-02 10:54:35 +0300

    hw 7.2 redid

--- 07_AdvancedOOPFeatures/AirportTest/src/Main.java
+++ 07_AdvancedOOPFeatures/AirportTest/src/Main.java
@@ -12,19 +12,18 @@ public class Main {
         Airport airport = Airport.getInstance();
         LocalDateTime today = LocalDateTime.now();
         LocalDateTime plusTwoHours = LocalDateTime.now().plusHours(2);
-        airport.getTerminals()
-                .forEach(flight -> flight.getFlights().stream()
-                        .filter(f -> f.getDate().getTime() >= Date.from(today.atZone(ZoneId.systemDefault()).toInstant()).getTime()
-                                && f.getDate().getTime() <= Date.from(plusTwoHours.atZone(ZoneId.systemDefault()).toInstant()).getTime()
-                                && f.getType().equals(Flight.Type.ARRIVAL))
-                        .map(a -> {
-                            a.getAircraft().getModel();
-                            a.getDate();
-                            return "Модель самолета: " + a.getAircraft().getModel() + ", Дата и время вылета : " +
-                                    new SimpleDateFormat("dd.MM.yyyy HH:mm").format(a.getDate());
-                        })
-                        .forEach(System.out::println));
+        airport.getTerminals().stream()
+                .flatMap(flight -> flight.getFlights().stream())
+                .filter(f -> f.getDate().getTime() >= Date.from(today.atZone(ZoneId.systemDefault()).toInstant()).getTime()
+                        && f.getDate().getTime() <= Date.from(plusTwoHours.atZone(ZoneId.systemDefault()).toInstant()).getTime()
+                        && f.getType().equals(Flight.Type.ARRIVAL))
+                .map(a -> {
+                    a.getAircraft().getModel();
+                    a.getDate();
+                    return "Модель самолета: " + a.getAircraft().getModel() + ", Дата и время вылета : " +
+                            new SimpleDateFormat("dd.MM.yyyy HH:mm").format(a.getDate());
+                })
+                .forEach(System.out::println);
     }
-
 }
 
