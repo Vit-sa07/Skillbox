0027bd23b7bbc7c106f1849cd188a72c22f4e75e
Vit-sa07 <57671251+Vit-sa07@users.noreply.github.com>
2020-10-20 15:25:22 +0300

    remade 9.4 lesson

--- 09_FilesAndNetwork/LentaHTML/src/Main.java
+++ 09_FilesAndNetwork/LentaHTML/src/Main.java
@@ -21,7 +21,7 @@ public class Main {
                     .userAgent("Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/69.0.3497.105 Mobile/15E148 Safari/605.1")
                     .maxBodySize(0)
                     .get();
-            Elements img = doc.getElementsByTag("img");
+            Elements img = doc.select("img[src~=(?i)\\.(png|jpe?g)]");
             ArrayList<String> urls = new ArrayList<>();
             img.forEach(element -> urls.add(element.absUrl("src")));
             urls.forEach(url -> System.out.println(url));
